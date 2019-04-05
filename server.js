d195bf41dd8e510122d1f7e4196555e944f66678
wastedpotential <andy@wastedpotential.com>
2019-04-05 16:46:50 -0400

    add email field to index form, continue stubbing out methods for users/

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+const Joi = require('joi');
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
-const db = require('./db_creds');
+app.use(express.static('public')); //public files folder
 
 //connect to mongo DB:
-const MongoClient = require('mongodb').MongoClient;
+const db = require('./db_creds');
 const uri = db.dbCredentials.uri;
-
-//public files folder:
-app.use(express.static('public'));
 
 const urlEncodedParser = bodyParser.urlencoded({
     extended: false
@@ -20,7 +20,7 @@ app.get('/users', function (req, res) {
     console.log("GET request for /users");
     MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client) {
         if(err) {
-             res.send('Error occurred while connecting to MongoDB Atlas...\n',err);
+            res.send('Error occurred while connecting to MongoDB Atlas...\n',err);
         }
         console.log('Connected...');
         const collection = client.db("golf_db").collection("users");
@@ -32,21 +32,29 @@ app.get('/users', function (req, res) {
             res.send(result);
         })
         client.close();
-     });
+    });
+
+    //return all users with enabled = true
+    // DO NOT DISPLAY enabled FLAG
 })
 
 //retrieve a single user by id
-app.get('/users/:userId', function (req, res) {
+app.get('/users/:id', function (req, res) {
+    console.log("GET request for user id = " + req.params.id);
+    res.send('Hello GET for user id = ' + req.params.id);
 
-    console.log("Got a GET request for /users");
-    res.send('Hello GET for user id = ' + req.params.userId);
+    //retrieve user from DB with id and enabled = true
+    //if fail, return error
+    //if success, return user data - DO NOT DISPLAY enabled FLAG
 })
 
 //create a user
 app.post('/users', urlEncodedParser, function(req, res) {
     let postData = {
         first_name: req.body.first_name,
-        last_name: req.body.last_name
+        last_name: req.body.last_name,
+        email: req.body.email,
+        enabled: true //used for soft delete. 
     }
     console.log(postData);
     MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client) {
@@ -64,17 +72,27 @@ app.post('/users', urlEncodedParser, function(req, res) {
         client.close();
      });
     res.end(JSON.stringify(postData)); //display the data on the page
+
+    //look for user with this email address
+    //if success - return error (user already exists)
+    //if fail, create new user, return user data - DO NOT DISPLAY enabled FLAG
 })
 
 //delete a user
-app.delete('/users/:userId', urlEncodedParser, function(req, res) {
-    console.log("Got a DELETE request for /users");
-    res.send("Hello DELETE for user id = " + req.params.userId);
+app.delete('/users/:id', urlEncodedParser, function(req, res) {
+    //console.log("Got a DELETE request for /users");
+    res.send("DELETE for user id = " + req.params.id);
+    //find user with this id and enabled = true
+    //if success, set enabled to false, return user - DO NOT DISPLAY enabled FLAG
+    //if fail, return error
 })
 
-app.put('/users/:userId', urlEncodedParser, function(req, res) {
+app.put('/users/:id', urlEncodedParser, function(req, res) {
     //console.log("Got an UPDATE request for /users");
-    res.send("Hello UPDATE for user id = " + req.params.userId);
+    res.send("UPDATE for user id = " + req.params.id);
+    //find user with this id and enabled = true
+    //if success, update, return user - DO NOT DISPLAY enabled FLAG
+    //if fail, error
 })
 
 //form page:
@@ -84,9 +102,9 @@ app.get('/index.html', function(req, res) {
 
 
 //start server:
-const server = app.listen(8081, function() {
+const port = process.env.PORT || 3000
+const server = app.listen(port, function() {
     const host = server.address().address;
     const port = server.address().port;
-
     console.log('example app listening at http://%s:%s', host, port);
 })
