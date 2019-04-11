6bf4a7a163715c05a73001fa4d974d91236fa609
wastedpotential <andy@wastedpotential.com>
2019-04-10 20:35:32 -0400

    add PUT method, clean up errors, check for duplicate email on POST

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@ const userSchema = new mongoose.Schema({
     last_name: String,
     email: String,
     isEnabled: {type: Boolean, default: true},
-    date_created: {type: Date, default: Date.now},
-    date_updated: Date
+    date_created: {type: Date, default: Date.now()},
+    date_updated: {type: Date, default: Date.now()}
 })
 const User = mongoose.model('User', userSchema);
 
@@ -21,10 +21,10 @@ router.get('/', async function (req, res) {
         const users = await User
             .find({isEnabled: true})
             .select('first_name last_name email')
-        res.send(users);
+        return res.send(users);
     }
     catch(err) {
-        res.status(404).send(err);
+        return res.status(404).send(err.message);
     }
 })
 
@@ -36,10 +36,10 @@ router.get('/:id', async function (req, res) {
         const user = await User
             .findOne({isEnabled: true, _id: req.params.id})
             .select('first_name last_name email')
-        res.send(user);
+        return res.send(user);
     }
     catch(err) {
-        res.status(404).send(err);
+        return res.status(404).send(err.message);
     }
 })
 
@@ -48,14 +48,21 @@ router.post('/', async function(req, res) {
     //console.log("POST request for /users);
 
     //validate input:
-    const validationSchema = {
-        first_name: Joi.string().min(1).required(),
-        last_name: Joi.string().min(2).required(),
-        email: Joi.string().email({minDomainSegments: 2}).required(),
-    };
-    const {error} = Joi.validate(req.body, validationSchema);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
+    result = validateUserData(req.body);
+    if (result.error) {
+        return res.status(400).send(result.error.details[0].message);
+    }
+
+    //look for existing user with this email:
+    try {
+        const userCount = await User
+            .countDocuments({ email: req.body.email});
+        if (userCount > 0) {
+            return res.status(400).send("this email address already exists");
+        }        
+    }
+    catch(err) {
+        return res.send(err.message);
     }
 
     //if everything validated, insert it:
@@ -63,10 +70,10 @@ router.post('/', async function(req, res) {
     try {
         const result = await newUser.save()
         //TODO: remove hidden fields from result
-        res.send(result);
+        return res.send(result);
     }
     catch(err) {
-        res.status(400).send(err);
+        return res.status(400).send(err.message);
     }
     
     //TODO:
@@ -77,28 +84,55 @@ router.post('/', async function(req, res) {
 
 //soft-delete a user
 router.delete('/:id', async function(req, res) {
-    //console.log("Got a DELETE request for /users");
     
     try {
         const user = await User
             .findOneAndUpdate({isEnabled: true, _id: req.params.id}, 
                 { $set: {
-                    isEnabled: false
+                    isEnabled: false,
+                    date_updated: Date.now
                 }})
-            .select('first_name last_name email')
-        res.send(user);
+            .select('first_name last_name email');
+        return res.send(user);
     }
     catch(err) {
-        res.status(400).send(err);
+        return res.status(400).send(err.message);
     }
 })
 
-router.put('/:id', function(req, res) {
-    //console.log("Got an UPDATE request for /users");
-    res.send("UPDATE for user id = " + req.params.id);
-    //find user with this id and enabled = true
-    //if success, update, return user - DO NOT DISPLAY enabled FLAG
-    //if fail, error
+router.put('/:id', async function(req, res) {
+    
+    //validate input:
+    result = validateUserData(req.body);
+    if (result.error) {
+        return res.status(400).send(result.error.details[0].message);
+    }
+
+    try {
+        let user = await User
+            .findOneAndUpdate({isEnabled: true, _id: req.params.id},
+            { $set: {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                date_updated: Date.now()
+            }}, { new: true });
+            //.select('first_name last_name email');
+        return res.send(user);
+    }
+    catch(err) {
+        return res.status(400).send(err.message);
+    }
 })
+
+function validateUserData(user) {
+    //validate input:
+    const validationSchema = {
+        first_name: Joi.string().min(1).required(),
+        last_name: Joi.string().min(2).required(),
+        email: Joi.string().email({minDomainSegments: 2}).required(),
+    };
+    return Joi.validate(user, validationSchema);
+}
 
 module.exports = router;
